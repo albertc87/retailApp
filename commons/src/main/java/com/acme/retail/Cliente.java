95c53d20580de63f6e9d2645a3b41304df29c96b
virgyam <virginia.araujo@ceiba.com.co>
2017-12-09 14:12:02 -0500

    Se agregan propiedades a modelo cliente y venta.

--- commons/src/main/java/com/acme/retail/Cliente.java
+++ commons/src/main/java/com/acme/retail/Cliente.java
@@ -1,4 +1,60 @@
 package com.acme.retail;
 
 public class Cliente {
+
+    private String id;
+    private String nombre;
+    private String apellidos;
+    private String direccion;
+    private String telefono;
+    private String email;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- commons/src/main/java/com/acme/retail/Venta.java
+++ commons/src/main/java/com/acme/retail/Venta.java
@@ -1,4 +1,62 @@
 package com.acme.retail;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Venta {
+
+    private String id;
+    private String id_cliente;
+    private String descripcion;
+    private String estado;
+    private String total;
+    private String fecha;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(String id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
 }
